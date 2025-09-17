142d1cdf5826c84a95d2737ccd7020dbfc9c0165
DominicDev <s0l0estudi4r@gmail.com>
2025-09-17 17:51:40 +0200

    v0.1.2

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,15 +6,11 @@ export default defineConfig({
   build: {
     assets: 'static'
   },
-  
+
   //Localhost
   site: 'http://localhost:4321/'
 
   //Deploy normal project
   // base: '/project-1020/',
   // site: 'https://20essentials.github.io/project-1020/'
-
-  //Deploy 20 Projects
-  // base: '/20-projects-with-css/',
-  // site: 'https://20essentials.github.io/20-projects-with-css/',
 });
--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,6 +1,8 @@
-export const $ = (el: string) => document.querySelector(el);
-export const $$ = (el: string) => document.querySelectorAll(el);
+export const $ = <T extends Element = HTMLElement>(el: string) =>
+  document.querySelector<T>(el);
+export const $$ = <T extends Element = HTMLElement>(el: string) =>
+  document.querySelectorAll<T>(el);
 
 export function baseUrl(path: string) {
-  return new URL(path.replace(/^\/+/, ""), import.meta.env.SITE).toString();
+  return new URL(path.replace(/^\/+/, ''), import.meta.env.SITE).toString();
 }
