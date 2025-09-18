0f243724ceb0a7e6fd79098731504e204921064d
DominicDev <s0l0estudi4r@gmail.com>
2025-09-18 23:12:47 +0200

    v0.1.3

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,7 +1,8 @@
-export const $ = <T extends Element = HTMLElement>(el: string) =>
-  document.querySelector<T>(el);
-export const $$ = <T extends Element = HTMLElement>(el: string) =>
-  document.querySelectorAll<T>(el);
+export const $ = <T extends Element = HTMLElement>(el: string): T =>
+  document.querySelector<T>(el) as T;
+export const $$ = <T extends Element = HTMLElement>(el: string): T[] => [
+  ...document.querySelectorAll<T>(el)
+];
 
 export function baseUrl(path: string) {
   return new URL(path.replace(/^\/+/, ''), import.meta.env.SITE).toString();
